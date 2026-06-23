ae9768de12eac541ca7ead8227b8b4194161f4d6
LorinLinov <lorinlinov@abv.bg>
2026-06-23 16:47:44 +0300

    Add home page view

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello, World!');
+import http from 'http';
+import fs from 'fs/promises';
+
+const server=http.createServer(async (req, res)=>{
+    const homePage=await fs.readFile('src/views/home/index.html', 'utf-8');
+
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write(homePage);
+
+   res.end()
+}).listen(5000,()=>{
+    console.log('Server running at http://localhost:5000/');
+});
