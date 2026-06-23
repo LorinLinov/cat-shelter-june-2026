99818a9f9f6d1edd79868fa40f3a8cc2b180f2cd
LorinLinov <lorinlinov@abv.bg>
2026-06-23 17:23:01 +0300

    Add style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import http from 'http';
 import fs from 'fs/promises';
 
-const server=http.createServer(async (req, res)=>{
-    const homePage=await fs.readFile('src/views/home/index.html', 'utf-8');
+const server = http.createServer(async (req, res) => {
+    console.log(req.url);
+    console.log(res.url);
+    if (req.url === '/styles/site.css') {
+        const style = await fs.readFile('src/styles/site.css', 'utf-8');
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(style);
+        res.end();
+        return;
+    }
 
-    res.writeHead(200,{'Content-Type':'text/html'});
+    const homePage = await fs.readFile('src/views/home/index.html', 'utf-8');
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(homePage);
 
-   res.end()
-}).listen(5000,()=>{
+    res.end()
+}).listen(5000, () => {
     console.log('Server running at http://localhost:5000/');
 });
