d84db7e043668efd816a5b041962cfa088d0c103
LorinLinov <lorinlinov@abv.bg>
2026-06-25 17:11:29 +0300

    New modifications for add, edit, delete

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import breeds from './breeds.js';
 
 
 const server = http.createServer(async (req, res) => {
-    if(req.method === 'POST' && req.url === '/cats/add-breed') {
+    if (req.method === 'POST' && req.url === '/cats/add-breed') {
         let formData = '';
         req.on('data', chunk => {
             formData += chunk.toString();
@@ -18,22 +18,70 @@ const server = http.createServer(async (req, res) => {
             const breedName = breedData.get('breed');
             breeds.push({ id: v4(), name: breedName });
         });
+
+        return res.writeHead(302, { 'Location': '/' }).end();
     }
 
-    // if(req.method === 'POST' && req.url === '/cats/add-cat') {
-    //     let formData = '';
-    //     req.on('data', chunk => {
-    //         formData += chunk.toString();
-    //     });
+    if (req.method === 'POST' && req.url === '/cats/add-cat') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
 
-    //     req.on('end', () => {
-    //         const catData = new URLSearchParams(formData);
-    //         const catName = catData.get('name');
-    //         const catDescription = catData.get('description');
-    //         const catBreed = catData.get('breed');
-    //         cats.push({ id: (cats.length + 1).toString(), name: catName, description: catDescription, breed: catBreed });
-    //     });
-    // }
+        req.on('end', () => {
+            const catData = new URLSearchParams(body);
+            console.log(catData);
+            const catName = catData.get('name');
+            const catImageUrl = catData.get('upload');
+            const catDescription = catData.get('description');
+            const catBreed = catData.get('breed');
+            cats.push({ id: v4(), imageUrl: catImageUrl, name: catName, description: catDescription, breed: catBreed });
+        });
+        return res.writeHead(302, { 'Location': '/' }).end();
+    }
+
+    if (req.method === "POST" && req.url.startsWith('/cats/edit-cat/')) {
+        console.log(req.url);
+        const catId = req.url.split('/').pop();
+        const catIndex = cats.findIndex(c => c.id === catId);
+        let body = '';
+
+        req.on("data", chunk => {
+            body += chunk.toString();
+        });
+
+        req.on("end", () => {
+
+            const catData = new URLSearchParams(body);
+            const catName = catData.get('name');
+            const catImageUrl = catData.get('upload');
+            const catDescription = catData.get('description');
+            const catBreed = catData.get('breed');
+
+            console.log(catId, catName, catImageUrl, catDescription, catBreed);
+            if (catIndex !== -1) {
+                cats[catIndex] = { id: catId, imageUrl: catImageUrl, name: catName, description: catDescription, breed: catBreed };
+            }
+
+            res.writeHead(302, { 'Location': '/' });
+            res.end();
+        });
+
+        return;
+    }
+
+    if(req.method === "POST" && req.url.startsWith('/cats/delete-cat/')) {
+        const catId = req.url.split('/').pop();
+        const catIndex = cats.findIndex(c => c.id === catId);
+
+        if (catIndex !== -1) {
+            cats.splice(catIndex, 1);
+            res.writeHead(302, { 'Location': '/' });
+            res.end();
+            return;
+        }
+    }
+
     if (req.url === '/styles/site.css') {
         const style = await fs.readFile('src/styles/site.css', 'utf-8');
         res.writeHead(200, { 'Content-Type': 'text/css' });
@@ -56,19 +104,42 @@ const server = http.createServer(async (req, res) => {
                     <p><span>Breed: </span>${cat.breed}</p>
                     <p><span>Description: </span>${cat.description}</p>
                     <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
+                        <li class="btn edit"><a href="/cats/edit-cat/${cat.id}">Change Info</a></li>
+                        <li class="btn delete"><a href="/cats/delete-cat/${cat.id}">New Home</a></li>
                     </ul>
                 </li>`).join('\n'));
     } else if (req.url === '/cats/add-breed') {
         htmlContent = await fs.readFile('src/views/addBreed.html', 'utf-8');
     } else if (req.url === '/cats/add-cat') {
         htmlContent = await fs.readFile('src/views/addCat.html', 'utf-8');
-    } else if (req.url === '/cats/add-breed') {
-        htmlContent = await fs.readFile('src/views/addBreed.html', 'utf-8');
-    } else if (req.url === '/cats/add-cat') {
-        htmlContent = await fs.readFile('src/views/addCat.html', 'utf-8');
-    }else{
+        const breedOptions = breeds.map(breed => `<option value="${breed.name}">${breed.name}</option>`).join('\n');
+        htmlContent = htmlContent.replace('{{breeds}}', breedOptions);
+    } else if (req.url.startsWith('/cats/edit-cat/')) {
+        const catId = req.url.split('/').pop();
+        const cat = cats.find(c => c.id === catId);
+        if (cat) {
+            htmlContent = await fs.readFile('src/views/editCat.html', 'utf-8');
+            htmlContent = htmlContent.replace('{{catId}}', cat.id);
+            htmlContent = htmlContent.replaceAll("{{catName}}", cat.name);
+            htmlContent = htmlContent.replace('{{catDescription}}', cat.description);
+            htmlContent = htmlContent.replace('{{catImageUrl}}', cat.imageUrl);
+            htmlContent = htmlContent.replace('{{breeds}}', breeds.map(breed => breed!==cat.breed ? `<option value="${breed.name}">${breed.name}</option>` : `<option value="${breed.name}" selected>${breed.name}</option>`).join('\n'));
+        } else {
+            res.writeHead(404);
+            res.end();
+            return;
+        }
+    } else if (req.url.startsWith('/cats/delete-cat/')) {
+        const catId = req.url.split('/').pop();
+        const cat = cats.find(c => c.id === catId);
+        htmlContent = await fs.readFile('src/views/catShelter.html', 'utf-8');
+
+        htmlContent = htmlContent.replace('{{catId}}', catId)
+                                .replace('{{catName}}', cat.name)
+                                .replace('{{catDescription}}', cat.description)
+                                .replace('{{catImageUrl}}', cat.imageUrl)
+                                .replace('{{catBreed}}', cat.breed);
+    } else {
         htmlContent = await fs.readFile('src/views/404.html', 'utf-8');
     }
 
